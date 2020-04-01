b28984a4694a1b5833feb73c4674d1b9271f8102
WuQinglong <snail.wu@foxmail.com>
2020-04-01 14:54:46 +0800

    Hello

--- demo/src/main/java/com/snailwu/demo/App.java
+++ demo/src/main/java/com/snailwu/demo/App.java
@@ -1,14 +1,42 @@
 package com.snailwu.demo;
 
-import java.util.ArrayList;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.*;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("-0-20200302120241-87I3JYTA5H0UCPKBS9V4".length());
-        String s = "user-o9jkh5WnaTjgZpy-HimytiW-h3sA-0-20200302120241-87I3JYTA5H0UCPKBS9V4";
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.clear(Calendar.SECOND);
+        calendar.clear(Calendar.MINUTE);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        System.out.println(DateFormatUtils.format(calendar.getTime(), "yyyy-MM-dd HH:mm:ss:sss"));
+        calendar.add(Calendar.YEAR, 1);
+        System.out.println(DateFormatUtils.format(calendar.getTime(), "yyyy-MM-dd HH:mm:ss:sss"));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(2);
+        list.add(6);
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("a", 1);
+        map.put("b", 3);
+        map.put("c", 2);
+        map.put("d", 4);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
     }
 }
