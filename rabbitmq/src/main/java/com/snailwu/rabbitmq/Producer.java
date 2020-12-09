56a01fea63a9533f826f3902c7e81e1f51d61793
WuQinglong <snail.wu@foxmail.com>
2020-12-09 10:32:19 +0800

    Commit.

--- rabbitmq/src/main/java/com/snailwu/rabbitmq/Producer.java
+++ rabbitmq/src/main/java/com/snailwu/rabbitmq/Producer.java
@@ -1,8 +1,6 @@
 package com.snailwu.rabbitmq;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -26,18 +24,54 @@ public class Producer {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-//        new AMQP.BasicProperties().builder()
+        // 启用消息确认模式
+        channel.confirmSelect();
 
+        // 添加一个确认监听（先监听，后发送消息）
+        channel.addConfirmListener(new ConfirmListener() {
+            /**
+             * ACK 确认
+             * @param deliveryTag 消息的唯一标签
+             * @param multiple 是否批量
+             * @throws IOException ERROR
+             */
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println("ConfirmListener ----- ACK! -----");
+            }
+
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println("ConfirmListener ----- NO ACK! -----");
+            }
+        });
+
+        // 添加不可达消息监听
+        channel.addReturnListener(new ReturnListener() {
+            @Override
+            public void handleReturn(int replyCode, String replyText, String exchange, String routingKey,
+                                     AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("ReturnListener Exchange: " + exchange + ", RoutingKey: " + routingKey);
+            }
+        });
+
+        // 发送消息
         for (int i = 0; i < 5; i++) {
             String message = "Hello RabbitMQ!";
-            channel.basicPublish("bus", "bus.cpu", null,
+            channel.basicPublish("bus", "bus.cpu", true, null,
                     message.getBytes(StandardCharsets.UTF_8));
-
             System.out.println("Producer Send '" + message + "'");
         }
 
-        channel.close();
-        connection.close();
+        channel.basicPublish("bus", "bus.cpu111", true, null,
+                "Hello ReturnListener".getBytes(StandardCharsets.UTF_8));
+        System.out.println("Producer Send 'Hello ReturnListener'");
+
+
+
+
+//        channel.close();
+//        connection.close();
     }
 
 }
