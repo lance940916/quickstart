b4d98f4af33e9d89c16c9201ea5561631e14cf11
WuQinglong <snail.wu@foxmail.com>
2020-12-14 14:12:57 +0800

    Commit.

---
+++ springboot/springboot-rabbitmq/src/test/java/com/snailwu/rabbitmq/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snailwu.rabbitmq;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
